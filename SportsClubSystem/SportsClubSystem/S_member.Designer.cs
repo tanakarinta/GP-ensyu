2617cfbb81a4c69f4427472c666143a1aaf146be
STD\s20217033 <20217033-tanakarinta@hcs.ac.jp>
2022-11-16 14:44:04 +0900

    db操作サンプル完成

--- SportsClubSystem/SportsClubSystem/S_member.Designer.cs
+++ SportsClubSystem/SportsClubSystem/S_member.Designer.cs
@@ -35,11 +35,25 @@ namespace SportsClubSystem
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(9, 9);
+            this.button1.Location = new System.Drawing.Point(30, 18);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(135, 70);
             this.button1.TabIndex = 0;
@@ -49,7 +63,7 @@ namespace SportsClubSystem
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(152, 9);
+            this.button2.Location = new System.Drawing.Point(173, 18);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(135, 70);
             this.button2.TabIndex = 1;
@@ -60,7 +74,7 @@ namespace SportsClubSystem
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(297, 37);
+            this.label1.Location = new System.Drawing.Point(318, 46);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 15);
             this.label1.TabIndex = 2;
@@ -69,7 +83,7 @@ namespace SportsClubSystem
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(472, 37);
+            this.label2.Location = new System.Drawing.Point(493, 46);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 15);
             this.label2.TabIndex = 3;
@@ -77,7 +91,7 @@ namespace SportsClubSystem
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(296, 57);
+            this.textBox1.Location = new System.Drawing.Point(317, 66);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(165, 22);
             this.textBox1.TabIndex = 4;
@@ -85,17 +99,149 @@ namespace SportsClubSystem
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(470, 57);
+            this.textBox2.Location = new System.Drawing.Point(491, 66);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(165, 22);
             this.textBox2.TabIndex = 5;
             this.textBox2.Text = "0";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(173, 95);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 70);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "データ読み込み";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(317, 94);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(445, 178);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(173, 280);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 70);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "データ修正";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(173, 362);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(135, 70);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "データ削除";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(317, 305);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(165, 22);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(318, 287);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "検索CD";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(317, 347);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(165, 22);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Text = "Name";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(491, 347);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(165, 22);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(318, 329);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 15);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "productname";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(493, 329);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 15);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "price";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(317, 410);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(165, 22);
+            this.textBox6.TabIndex = 16;
+            this.textBox6.Text = "1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(318, 392);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 15);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "検索CD";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(30, 95);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(135, 70);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "テーブル削除";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // S_member
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -104,6 +250,7 @@ namespace SportsClubSystem
             this.Controls.Add(this.button1);
             this.Name = "S_member";
             this.Text = "データベース";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,5 +264,18 @@ namespace SportsClubSystem
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button6;
     }
 }
--- SportsClubSystem/SportsClubSystem/S_member.cs
+++ SportsClubSystem/SportsClubSystem/S_member.cs
@@ -56,5 +56,85 @@ namespace SportsClubSystem
                 }
             }
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using(SQLiteConnection con = new SQLiteConnection("Data Source=member.db"))
+            {
+                //DataTableを生成
+                var dataTabel = new DataTable();
+                //SQLの実行
+                var adapter = new SQLiteDataAdapter("SELECT * FROM t_product", con);
+                adapter.Fill(dataTabel);
+                dataGridView1.DataSource = dataTabel;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (SQLiteConnection con = new SQLiteConnection("Data Source=member.db"))
+            {
+                con.Open();
+                using (SQLiteTransaction trans = con.BeginTransaction())
+                {
+                    SQLiteCommand cmd = con.CreateCommand();
+                    //インサート
+                    cmd.CommandText = "UPDATE t_product set productname = @Product, price = @Price WHERE CD = @Cd";
+                    //パラメータセット
+                    cmd.Parameters.Add("Product", System.Data.DbType.String);
+                    cmd.Parameters.Add("Price", System.Data.DbType.Int64);
+                    cmd.Parameters.Add("Cd", System.Data.DbType.Int64);
+                    //データ追加
+                    cmd.Parameters["Product"].Value = textBox3.Text;
+                    cmd.Parameters["Price"].Value = int.Parse(textBox4.Text);
+                    cmd.Parameters["Cd"].Value = int.Parse(textBox5.Text);
+                    cmd.ExecuteNonQuery();
+                    //コミット
+                    trans.Commit();
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (SQLiteConnection con = new SQLiteConnection("Data Source=member.db"))
+            {
+                con.Open();
+                using (SQLiteTransaction trans = con.BeginTransaction())
+                {
+                    SQLiteCommand cmd = con.CreateCommand();
+                    //インサート
+                    cmd.CommandText = "DELETE FROM t_product WHERE CD = @Cd";
+                    //パラメータセット
+                    cmd.Parameters.Add("Cd", System.Data.DbType.Int64);
+                    //データ削除
+                    cmd.Parameters["Cd"].Value = int.Parse(textBox6.Text);
+                    cmd.ExecuteNonQuery();
+                    //コミット
+                    trans.Commit();
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //コネクションを開いてテーブル削除して閉じる
+            using (var con = new SQLiteConnection("Data Source=member.db"))
+            {
+                con.Open();
+                using (SQLiteCommand command = con.CreateCommand())
+                {
+                    command.CommandText =
+                        "drop table t_product";
+                    command.ExecuteNonQuery();
+                }
+                con.Close();
+            }
+        }
     }
 }
